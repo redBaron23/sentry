1788d0b53dae1609ede8abd41baf42cf846cdc88
Pato <23110463+redBaron23@users.noreply.github.com>
2024-09-04 01:46:06 -0300

    add paper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import { RadialChart } from "@/components/atoms/charts/radial-chart";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-slate-50">
-      <RadialChart name="Server" value={95} />
+      <div className="flex gap-3">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Parches de seguridad (SCCM)</CardTitle>
+          </CardHeader>
+          <CardContent className="flex">
+            <RadialChart title="Server" name="Server" value={55} />
+            <RadialChart title="Work Station" name="Work Station" value={55} />
+          </CardContent>
+        </Card>
+
+        <Card className="">
+          <CardHeader>
+            <CardTitle>Parches de seguridad (SCCM)</CardTitle>
+          </CardHeader>
+          <CardContent className="flex">
+            <RadialChart name="Server" value={95} />
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
--- src/components/atoms/charts/radial-chart.tsx
+++ src/components/atoms/charts/radial-chart.tsx
@@ -17,6 +17,7 @@ import {
 interface RadialChartProps {
   name: string;
   value: number;
+  title?: string;
   color?: string;
   maxWidth?: number;
   startAngle?: number;
@@ -28,6 +29,7 @@ interface RadialChartProps {
 export function RadialChart({
   name,
   value,
+  title,
   color = "hsl(142, 76%, 36%)",
   maxWidth = 200,
   startAngle = 180,
@@ -50,74 +52,79 @@ export function RadialChart({
   };
 
   return (
-    <ChartContainer
-      config={chartConfig}
-      className={`mx-auto aspect-square w-full max-w-[${maxWidth}px]`}
-    >
-      <RadialBarChart
-        data={chartData}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        innerRadius={innerRadius}
-        outerRadius={outerRadius}
-      >
-        <PolarAngleAxis
-          type="number"
-          domain={[0, 100]}
-          angleAxisId={0}
-          tick={false}
-        />
-        <RadialBar
-          background
-          dataKey="value"
-          // fill={`var(--color-${name})`}
-          cornerRadius={5}
-          className="stroke-transparent stroke-2 fill-green-700"
-        />
-        <ChartTooltip
-          cursor={false}
-          content={<ChartTooltipContent nameKey={name} />}
-        />
-        <PolarRadiusAxis tick={false} axisLine={false}>
-          <Label
-            content={({ viewBox }) => {
-              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                return (
-                  <>
-                    <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle">
-                      <tspan
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        className="fill-foreground text-2xl font-bold"
-                      >
-                        {value}%
-                      </tspan>
-                    </text>
-                    <text x={viewBox.cx} y={viewBox.cy}>
-                      <tspan
-                        x={(viewBox.cx || 0) - 90}
-                        y={(viewBox.cy || 0) + 20}
-                        textAnchor="start"
-                        className="fill-muted-foreground text-xs"
-                      >
-                        0%
-                      </tspan>
-                      <tspan
-                        x={(viewBox.cx || 0) + 100}
-                        y={(viewBox.cy || 0) + 20}
-                        textAnchor="end"
-                        className="fill-muted-foreground text-xs"
-                      >
-                        100%
-                      </tspan>
-                    </text>
-                  </>
-                );
-              }
-            }}
-          />
-        </PolarRadiusAxis>
-      </RadialBarChart>
-    </ChartContainer>
+    <div className="flex flex-col items-center">
+      {title && <h5>{title}</h5>}
+      <div className={`w-full min-w-[${maxWidth}px]`}>
+        <ChartContainer
+          config={chartConfig}
+          className={`mx-auto aspect-square  max-w-[${maxWidth}px]`}
+        >
+          <RadialBarChart
+            data={chartData}
+            startAngle={startAngle}
+            endAngle={endAngle}
+            innerRadius={innerRadius}
+            outerRadius={outerRadius}
+          >
+            <PolarAngleAxis
+              type="number"
+              domain={[0, 100]}
+              angleAxisId={0}
+              tick={false}
+            />
+            <RadialBar
+              background
+              dataKey="value"
+              // fill={`var(--color-${name})`}
+              cornerRadius={5}
+              className="stroke-transparent stroke-2 fill-green-700"
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent nameKey={name} />}
+            />
+            <PolarRadiusAxis tick={false} axisLine={false}>
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <>
+                        <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle">
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-2xl font-bold"
+                          >
+                            {value}%
+                          </tspan>
+                        </text>
+                        <text x={viewBox.cx} y={viewBox.cy}>
+                          <tspan
+                            x={(viewBox.cx || 0) - 90}
+                            y={(viewBox.cy || 0) + 20}
+                            textAnchor="start"
+                            className="fill-muted-foreground text-xs"
+                          >
+                            0%
+                          </tspan>
+                          <tspan
+                            x={(viewBox.cx || 0) + 100}
+                            y={(viewBox.cy || 0) + 20}
+                            textAnchor="end"
+                            className="fill-muted-foreground text-xs"
+                          >
+                            100%
+                          </tspan>
+                        </text>
+                      </>
+                    );
+                  }
+                }}
+              />
+            </PolarRadiusAxis>
+          </RadialBarChart>
+        </ChartContainer>
+      </div>
+    </div>
   );
 }
